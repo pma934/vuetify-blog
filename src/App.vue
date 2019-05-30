c1065756c3e5cba340db52938a4ba0a337013c58
Zhou Xuanzhuo <358409584@qq.com>
2019-05-31 01:54:28 +0800

    use theme

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,92 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
+  <v-app id="sandbox" :dark="dark">
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      :permanent="primaryDrawer.type === 'permanent'"
+      :temporary="primaryDrawer.type === 'temporary'"
+      :clipped="primaryDrawer.clipped"
+      :floating="primaryDrawer.floating"
+      :mini-variant="primaryDrawer.mini"
+      absolute
+      overflow
+      app
+    ></v-navigation-drawer>
+    <v-toolbar :clipped-left="primaryDrawer.clipped" app absolute>
+      <v-toolbar-side-icon
+        v-if="primaryDrawer.type !== 'permanent'"
+        @click.stop="primaryDrawer.model = !primaryDrawer.model"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
     </v-toolbar>
-
     <v-content>
-      <router-view/>
+      <v-container fluid>
+        <v-layout align-center justify-center>
+          <v-flex xs10>
+            <v-card>
+              <v-card-text>
+                <v-layout row wrap>
+                  <v-flex xs12 md6>
+                    <span>Scheme</span>
+                    <v-switch v-model="dark" primary label="Dark"></v-switch>
+                  </v-flex>
+                  <v-flex xs12 md6>
+                    <span>Drawer</span>
+                    <v-radio-group v-model="primaryDrawer.type" column>
+                      <v-radio
+                        v-for="drawer in drawers"
+                        :key="drawer"
+                        :label="drawer"
+                        :value="drawer.toLowerCase()"
+                        primary
+                      ></v-radio>
+                    </v-radio-group>
+                    <v-switch v-model="primaryDrawer.clipped" label="Clipped" primary></v-switch>
+                    <v-switch v-model="primaryDrawer.floating" label="Floating" primary></v-switch>
+                    <v-switch v-model="primaryDrawer.mini" label="Mini" primary></v-switch>
+                  </v-flex>
+                  <v-flex xs12 md6>
+                    <span>Footer</span>
+                    <v-switch v-model="footer.inset" label="Inset" primary></v-switch>
+                  </v-flex>
+                </v-layout>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn flat>Cancel</v-btn>
+                <v-btn flat color="primary">Submit</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
+    <v-footer :inset="footer.inset" app>
+      <span class="px-3">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data () {
-    return {
-      //
+  data: () => ({
+    dark: false,
+    drawers: ["Default (no property)", "Permanent", "Temporary"],
+    primaryDrawer: {
+      model: null,
+      type: "default (no property)",
+      clipped: false,
+      floating: false,
+      mini: false
+    },
+    footer: {
+      inset: false
+    }
+  }),
+  computed: {
+    aaa() {
+      console.log(this.dark);
+      return this.dark;
     }
   }
-}
+};
 </script>
