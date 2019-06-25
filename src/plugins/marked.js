75fbbfae2da561f2ffe47619b146c13fcb070f24
Zhou Xuanzhuo <358409584@qq.com>
2019-06-25 10:50:45 +0800

    fix   解决highlight is not defined

--- src/plugins/marked.js
+++ src/plugins/marked.js
@@ -3,14 +3,12 @@ import marked from "marked";
 
 import '../assets/css/markdown.scss';
 
-import hljs from 'highlight.js';
-
 Vue.prototype.$marked = marked
 
 marked.setOptions({
   renderer: new marked.Renderer(),
   highlight: function (code) {
-    return hljs.highlightAuto(code).value;
+    return require('highlight.js').highlightAuto(code).value;
   },
   gfm: true,
   tables: true,
